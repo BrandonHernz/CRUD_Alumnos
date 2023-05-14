7d04388219785e502b43914415829d55b7f7caad
Brandon Isahir Hernández Camacho <brandon.ihernz@gmail.com>
2023-05-14 00:47:28 -0600

    Actualizar header

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -1,28 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CRUD Libros</title>
-</head>
-
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-sm">
-        <a class="navbar-brand" href="<?= base_url('/') ?>">
-          <img src="imagenes/Alumno_crud.svg" alt="Logo" height="30" class="d-inline-block align-text-top">
-        </a>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="<?= base_url('Alumnos') ?>">Alumnos</a>
-            </li>
-          </ul>
-        </div>
+<header>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-sm">
+      <a class="navbar-brand" href="<?= base_url('/') ?>">
+        <img src="imagenes/Alumno_crud.svg" alt="Logo" height="30" class="d-inline-block align-text-top">
+      </a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('Alumnos') ?>">Alumnos</a>
+          </li>
+        </ul>
       </div>
-    </nav>
-  </header>
-</html>
-
+    </div>
+  </nav>
+</header>
