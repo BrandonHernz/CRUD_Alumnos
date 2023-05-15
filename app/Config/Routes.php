406f684992c35aed7b7fafecd126438103670e53
Brandon Isahir Hernández Camacho <brandon.ihernz@gmail.com>
2023-05-15 11:12:22 -0600

    Actualizar rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,7 +11,7 @@ $routes = Services::routes();
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('AlumnoController');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
@@ -29,9 +29,9 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
-$routes->get('Alumnos', 'Alumno::index');
-$routes->post('registrarAlumno', 'Alumno::guardarAlumno');
+$routes->get('/', 'AlumnoController::home');
+$routes->get('Alumnos', 'AlumnoController::tableStudents');
+$routes->post('registrarAlumno', 'AlumnoController::saveStudent');
 
 /*
  * --------------------------------------------------------------------
